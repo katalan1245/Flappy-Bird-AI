57d16c2b8276318bec6c7e52059f32267bb942d3
katalan1 <katalan1245@users.noreply.github.com>
2020-06-19 13:57:04 +0300

    Updated config-feedforward.txt

--- flappy.py
+++ flappy.py
@@ -1,14 +1,21 @@
 import pygame
 import neat
-import time
 import os
 import random
-import sys
-pygame.font.init()
+import pickle
+from sys import exit
+from time import sleep
+
+pygame.font.init() # font init
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+GEN = 0
+ALIVE = 0
+
+TRAIN = False
+SAVE = False
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
              pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
@@ -159,7 +166,7 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
     
 
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen, alive):
     win.blit(BG_IMG, (0,0))
     
     for pipe in pipes:
@@ -168,15 +175,37 @@ def draw_window(win, bird, pipes, base, score):
     text = STAT_FONT.render('Score: ' + str(score), 1, (255,255,255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
     
+    text = STAT_FONT.render('Gen: ' + str(gen), 1, (255,255,255))
+    win.blit(text, (10, 10))
+    
+    text = STAT_FONT.render('Alive: ' + str(alive), 1, (255,255,255))
+    win.blit(text, (10, 50))
+    
     base.draw(win)
     
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
+        
     pygame.display.update()
     
     
 
-def main():
-    bird = Bird(230, 350)
+def main(genomes, config):
+    global GEN
+    GEN += 1
+    nets = []  # The neural networks
+    ge = []    # The real birds
+    birds = [] # The game objects
+    stop = True
+    
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+        
+    
     base = Base(730)
     pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -185,42 +214,138 @@ def main():
     score = 0
     
     run = True
+    
+    if len(genomes) == 1:
+        stop = False
+    
     while run:
-        clock.tick(30)
+        ALIVE = len(birds)
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                exit()
                 
-        #bird.move()
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+                
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+            
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+            
+            if output[0] > 0.5:
+                bird.jump()
+
+        
         add_pipe = False
         rem = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
-            
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+                    
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
-                rem.append(pipe) 
-            
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
+                rem.append(pipe)   
                 
             pipe.move()
             
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fitness += 5
             pipes.append(Pipe(600))
             
         for r in rem:
             pipes.remove(r)
         
-        if bird.y + bird.img.get_height() >= 730:
-            pass
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+        
+        if stop and score > 50:
+            pygame.quit()
+            break
         
         base.move()
-        draw_window(win, bird, pipes, base, score)
+        draw_window(win, birds, pipes, base, score, GEN, ALIVE)
+    
         
-    pygame.quit()
-    sys.exit()
 
-main()
+def run(config_path):
+    train, save = option()
+
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    
+    p = neat.Population(config)
+    
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    
+    if train:
+        winner = p.run(main,50)
+        print('Model Trained!')
+    
+    else:
+        
+        if os.path.exists('winner.pkl'): # check if file exist
+            
+            with open('winner.pkl', 'rb') as f:
+                genome = pickle.load(f) # load the trained neural network
+            
+            genomes = [(1, genome)]
+            main(genomes, config) # run the game with the best genome
+        
+        else:
+            print('Trained Neural Network File Doesn\'t Exist!')
+            quit()
+    
+    if save:
+        with open('winner.pkl', 'wb') as f:
+            pickle.dump(winner, f)
+            print('Model Saved on: "winner.pkl"')
+        
+
+def option():
+    print('Welcome to AI Flappy Bird!')
+    print('Choose Your Option:')
+    print('1. Train The Module')
+    print('2. Train And Save The Model')
+    print('3. Load your own model')
+    n = input('Enter Option Num: ')
+    
+    while n not in ['1','2','3']:
+        print('Choose only 1, 2 or 3')
+        n = input('Enter Option Num: ')
+    
+    if n == '1':
+        return True, False
+    elif n == '2':
+        return True, True
+    
+    return False, False
+    
+    
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
